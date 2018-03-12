4da673188a3089b6b0c0e13868b9d7544d030a40
ted <ted@teddeiMac.mshome.net>
2018-03-12 23:50:37 +0800

    sydw exam

--- pages/index/chooseCourse/chooseCourse.js
+++ pages/index/chooseCourse/chooseCourse.js
@@ -10,6 +10,7 @@ Page({
   data: {
     course: null,
     subtypeHiddenIndex: 0,
+    basetypeHiddenIndex:0,
   },
 
   /**
@@ -23,14 +24,16 @@ Page({
       course: temp
     })
   },
+setCourseId: function(e) {
 
+},
   chooseNext: function (e) {
     //console.log(e.currentTarget.dataset.categoryid)
     //console.log(e.currentTarget.dataset.index)
     //console.log(this.data.course[e.currentTarget.dataset.index].isHidden)
-//    console.log(this.data.course[e.currentTarget.dataset.index].onlyOneSubtype)
-    if (this.data.course[e.currentTarget.dataset.index].onlyOneSubtype==1) {
-//      console.log(this.data.course[e.currentTarget.dataset.index].subType[0].baseType[0].baseTypeId)
+    //    console.log(this.data.course[e.currentTarget.dataset.index].onlyOneSubtype)
+    if (this.data.course[e.currentTarget.dataset.index].onlyOneSubtype == 1) {
+      //      console.log(this.data.course[e.currentTarget.dataset.index].subType[0].baseType[0].baseTypeId)
       wx.checkSession({
         success: res => {
           var basetypeid = this.data.course[e.currentTarget.dataset.index].subType[0].baseType[0].baseTypeId
@@ -56,7 +59,7 @@ Page({
             }
           })
         }
-      })      
+      })
     } else {
       if (this.data.course[e.currentTarget.dataset.index].isHidden) {
         this.data.course[e.currentTarget.dataset.index].isHidden = false
@@ -97,20 +100,85 @@ Page({
 
   chooseSubCourses: function (e) {
     //   console.log(res)
-    if (this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden) {
-      this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden = false
-      for (var i = 0; i < this.data.course[this.data.subtypeHiddenIndex].subType.length; i++) {
-        if (i != e.currentTarget.dataset.index) {
-          this.data.course[this.data.subtypeHiddenIndex].subType[i].isHidden = true
+
+    //subtypeHiddenIndex代表当前选中哪个一级菜单(英语／公务员／事业单位...)
+    if (this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].onlyOneBaseType==1) {
+      wx.checkSession({
+        success: res => {
+          var basetypeid = this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].baseType[0].baseTypeId
+          wx.request({
+            url: app.globalData.url + 'wxlogin/getuserInfo.php?which=setchooseCourse&baseTypeId=' + basetypeid + '&userId=' + app.globalData.userId + '&session_key=' + app.globalData.session_key,
+            success: res => {
+              //    console.log(res)
+              wx.setStorageSync('currentCourse', basetypeid)
+              wx.showToast({
+                title: '选课成功',
+              })
+              wx.redirectTo({
+                url: '../index'
+              })
+            },
+            fail: res => {
+              wx.showToast({
+                title: '登录状态异常',
+              })
+              wx.redirectTo({
+                url: '../index',
+              })
+            }
+          })
         }
-      }
+      })
     } else {
-      this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden = true
+      //basetypeHiddenIndex代表选择了哪个2级菜单(理工/综合/卫生...)
+      this.data.basetypeHiddenIndex = e.currentTarget.dataset.index
+      if (this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden) {
+        this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden = false
+        for (var i = 0; i < this.data.course[this.data.subtypeHiddenIndex].subType.length; i++) {
+          if (i != e.currentTarget.dataset.index) {
+            this.data.course[this.data.subtypeHiddenIndex].subType[i].isHidden = true
+          }
+        }
+      } else {
+        this.data.course[this.data.subtypeHiddenIndex].subType[e.currentTarget.dataset.index].isHidden = true
+      }
+
+      this.setData({
+        course: this.data.course,
+        basetypeHiddenIndex: this.data.basetypeHiddenIndex,
+        
+      })
     }
 
-    this.setData({
-      course: this.data.course
-    })
+  },
+
+  chooseBaseCourses : function (e) {
+    wx.checkSession({
+      success: res => {
+        var basetypeid = this.data.course[this.data.subtypeHiddenIndex].subType[this.data.basetypeHiddenIndex].baseType[e.currentTarget.dataset.index].baseTypeId
+        wx.request({
+          url: app.globalData.url + 'wxlogin/getuserInfo.php?which=setchooseCourse&baseTypeId=' + basetypeid + '&userId=' + app.globalData.userId + '&session_key=' + app.globalData.session_key,
+          success: res => {
+            //    console.log(res)
+            wx.setStorageSync('currentCourse', basetypeid)
+            wx.showToast({
+              title: '选课成功',
+            })
+            wx.redirectTo({
+              url: '../index'
+            })
+          },
+          fail: res => {
+            wx.showToast({
+              title: '登录状态异常',
+            })
+            wx.redirectTo({
+              url: '../index',
+            })
+          }
+        })
+      }
+    })   
   },
   callback: function (res) {
     //        console.log('cc:'+res.data)
